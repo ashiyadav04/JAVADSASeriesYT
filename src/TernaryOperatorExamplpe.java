5e9fde35fcc6107ad784af7cf28f3c38921322f2
ashiyadav04 <ashiyadav959@gmail.com>
2026-04-22 21:48:27 +0530

    Removed .idea and added gitignore

--- src/TernaryOperatorExamplpe.java
+++ src/TernaryOperatorExamplpe.java
@@ -5,7 +5,7 @@ public class TernaryOperatorExamplpe {
      Scanner sc=new Scanner(System.in);
         System.out.println("Enter value of a:");
      int a=sc.nextInt();
-    String b=(a>=18)?("you can vote"):("You can't vote");
+    String b=(a>=18)?("you can vote"):("You can't vote");//Ternary operator condition?(yes):(no);
         System.out.println(b);
     }
 }
